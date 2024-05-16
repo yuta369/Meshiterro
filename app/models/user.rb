bcce5658d5ae142673562f87721345e4fed126ef
yuta369 <y.m.36912@gmail.com>
2024-05-16 12:28:44 +0000

    [Finish] 4-17 [Update] 投稿機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,22 @@ class User < ApplicationRecord
          :recoverable,              # パスワードをリセット
          :rememberable,             # ログイン情報を保存
          :validatable               # email のフォーマットなどのバリデーション
+
   # アソシエーション ( 1:N の「1」側 )
   has_many :post_images, dependent: :destroy
+
+  has_one_attached :profile_image
+
+  def get_profile_image(width, height)
+    # プロフィール画像が添付されていない場合
+    unless profile_image.attached?
+      # デフォルトプロフィール画像のファイルパスを設定
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      # デフォルトプロフィール画像をprofile_imageに添付
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    # 画像サイズの変更
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # ルーティングを一括して自動生成, 生成するルーティングの限定
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   devise_for :users  # devise を使用する際に URL として users を含む
   root to: "homes#top"
   get "homes/about", to: "homes#about", as: "about"
+  # ルーティングを一括して自動生成, 生成するルーティングの限定
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
 end
